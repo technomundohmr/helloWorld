f234d6c3ee9e2d2b75190344be6fe2a8c38c5720
Andres Rincon <andresf.rincong@gmail.com>
2023-04-20 09:55:54 -0500

    validation body

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
+class Location(BaseModel):
+    city: str
+    country: str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -57,3 +61,18 @@ def show_person(
     return {
         person_id: "existed",
     }
+    
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int= Path(
+        ...,
+        title="Person ID",
+        description="this is te person ID",
+        gt=0,
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    result = person.dict()
+    data = result.update(location.dict())
+    return data
